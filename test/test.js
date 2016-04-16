1e1c4c1911b6eae25fe427f642290ccd8000351e
Felix Zapata <fzberlinches@gmail.com>
2016-04-16 17:16:46 +0200

    test(test.js): enable first test for domain

--- test/test.js
+++ test/test.js
@@ -13,6 +13,25 @@ require('mocha');
 
 var fixtures = function(glob) { return path.join(__dirname, './.tmp', glob); }
 
+var keywords = [
+	'__:1,2d',
+	'_e:1,2d',
+	'_x:1,2c,3d',
+	'esc_html__:1,2d',
+	'esc_html_e:1,2d',
+	'esc_html_x:1,2c,3d',
+	'esc_attr__:1,2d', 
+	'esc_attr_e:1,2d', 
+	'esc_attr_x:1,2c,3d', 
+	'_ex:1,2c,3d',
+	'_n:1,2,4d', 
+	'_nx:1,2,4c,5d',
+	'_n_noop:1,2,3d',
+	'_nx_noop:1,2,3c,4d'
+];
+
+
+var fixtures = function(glob) { return path.join(__dirname, './temp', glob); }
 
 function fileExists(filePath) {
   try {
@@ -23,40 +42,76 @@ function fileExists(filePath) {
 }
 
 describe('gulp-checktextdomain', function() {
-
   
+  var tmpFolder = path.join(__dirname, 'temp');
+
+  beforeEach(function(done) {
+    var folder = path.join(__dirname, './fixtures/');
+    fs.copy(folder, tmpFolder, function(err) {
+      done();
+    });
+  });
 
   // We'll delete it when we're done.
   afterEach(function(done) {
     fs.remove(tmpFolder, done);
   });
 
-  it('1) incorrect_domain_autocorrect', function(done) {
+
+  it('1) Domain should have been corrected', function(done) {
     
+      var actual;
+      var corrected;
+      var expected;
+      var options = {
+        force: true,
+        text_domain: 'my-domain',
+        correct_domain: true,
+        create_report_file: true,
+        keywords: keywords
+      };
+      
+      gulp.src(fixtures('incorrect-domain-autocorrect.php'))
+      .pipe(checktextdomain(options))
+      .pipe(sassert.first(function(d) {
+        //There are 14 missing domains
+        actual = JSON.parse(fs.readFileSync( '.incorrect_domain_autocorrect.json' ) );
+        actual['test/temp/incorrect-domain-autocorrect.php'].length.should.equal(14);
+        
+        //Test corrected file
+        corrected = fs.readFileSync( 'test/temp/incorrect-domain-autocorrect.php' ).toString();
+        expected = fs.readFileSync( 'test/expected/incorrect-domain-autocorrect.php' ).toString();
+        corrected.should.equal(expected);
+        
+        //Clean up: Delete report file
+        fs.remove( '.incorrect_domain_autocorrect.json' );
+      }))
+      .pipe(sassert.end(done));
+      
 
   });
 
-  it('2) variable_domain_autocomplete', function(done) {
+  xit('2) variable_domain_autocomplete', function(done) {
     
   });
 
-  it('3) missing_domain', function(done) {
+  xit('3) missing_domain', function(done) {
     
 
 
   });
 
-  it('4) missing_domain_ignore_missing', function(done) {
+  xit('4) missing_domain_ignore_missing', function(done) {
 
    
   });
   
-  it('5) correct_domain', function(done) {
+  xit('5) correct_domain', function(done) {
 
    
   });
   
-  it('6) plurals', function(done) {
+  xit('6) plurals', function(done) {
 
    
   });
