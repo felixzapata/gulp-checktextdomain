f6307850bd6c38a85ced667f16279ce3b535ab01
Felix Zapata <fzberlinches@gmail.com>
2017-08-12 17:33:04 +0200

    style(test.js): format code

--- test/test.js
+++ test/test.js
@@ -10,7 +10,7 @@ var assert = require('assert');
 var sassert = require('stream-assert');
 require('mocha');
 
-var fixtures = function(glob) { return path.join(__dirname, './temp', glob); }
+var fixtures = function (glob) { return path.join(__dirname, './temp', glob); }
 
 var keywords = [
   '__:1,2d',
@@ -29,24 +29,24 @@ var keywords = [
   '_nx_noop:1,2,3c,4d'
 ];
 
-describe('gulp-checktextdomain', function() {
+describe('gulp-checktextdomain', function () {
 
   var tmpFolder = path.join(__dirname, 'temp');
 
-  beforeEach(function(done) {
+  beforeEach(function (done) {
     var folder = path.join(__dirname, './fixtures/');
-    fs.copy(folder, tmpFolder, function(err) {
+    fs.copy(folder, tmpFolder, function (err) {
       done();
     });
   });
 
   // We'll delete it when we're done.
-  afterEach(function(done) {
+  afterEach(function (done) {
     fs.remove(tmpFolder, done);
   });
 
 
-  it('1) Domain should have been corrected', function(done) {
+  it('1) Domain should have been corrected', function (done) {
 
     var actual;
     var corrected;
@@ -61,7 +61,7 @@ describe('gulp-checktextdomain', function() {
 
     gulp.src(fixtures('incorrect-domain-autocorrect.php'))
       .pipe(checktextdomain(options))
-      .pipe(sassert.first(function(d) {
+      .pipe(sassert.first(function (d) {
         //There are 14 missing domains
         actual = JSON.parse(fs.readFileSync('.incorrect-domain-autocorrect.json'));
         actual[path.join(__dirname, 'temp/incorrect-domain-autocorrect.php')].length.should.equal(14);
@@ -79,7 +79,7 @@ describe('gulp-checktextdomain', function() {
 
   });
 
-  it('2) Domain should have been corrected', function(done) {
+  it('2) Domain should have been corrected', function (done) {
     var actual;
     var corrected;
     var expected;
@@ -93,7 +93,7 @@ describe('gulp-checktextdomain', function() {
 
     gulp.src(fixtures('variable-domain-autocorrect.php'))
       .pipe(checktextdomain(options))
-      .pipe(sassert.first(function(d) {
+      .pipe(sassert.first(function (d) {
         //There are 14 missing domains
         actual = JSON.parse(fs.readFileSync('.variable-domain-autocorrect.json'));
         actual[path.join(__dirname, 'temp/variable-domain-autocorrect.php')].length.should.equal(14);
@@ -109,7 +109,7 @@ describe('gulp-checktextdomain', function() {
       .pipe(sassert.end(done));
   });
 
-  it('3) Should detect missing domain', function(done) {
+  it('3) Should detect missing domain', function (done) {
     var actual;
     var corrected;
     var expected;
@@ -122,7 +122,7 @@ describe('gulp-checktextdomain', function() {
 
     gulp.src(fixtures('missing-domain.php'))
       .pipe(checktextdomain(options))
-      .pipe(sassert.first(function(d) {
+      .pipe(sassert.first(function (d) {
         //There are 14 missing domains
         actual = JSON.parse(fs.readFileSync('.missing-domain.json'));
         actual[path.join(__dirname, 'temp/missing-domain.php')].length.should.equal(14);
@@ -135,7 +135,7 @@ describe('gulp-checktextdomain', function() {
 
   });
 
-  it('4) Should ignore one missing domain', function(done) {
+  it('4) Should ignore one missing domain', function (done) {
     var actual;
     var corrected;
     var expected;
@@ -149,7 +149,7 @@ describe('gulp-checktextdomain', function() {
 
     gulp.src(fixtures('missing-domain.php'))
       .pipe(checktextdomain(options))
-      .pipe(sassert.first(function(d) {
+      .pipe(sassert.first(function (d) {
         //File only has missing domain, which we are nor reporting. There should be no errors.
         actual = JSON.parse(fs.readFileSync('.missing-domain.json'));
         actual[path.join(__dirname, 'temp/missing-domain.php')].length.should.equal(0);
@@ -161,7 +161,7 @@ describe('gulp-checktextdomain', function() {
 
   });
 
-  it('5) Should review that the domain is ok', function(done) {
+  it('5) Should review that the domain is ok', function (done) {
     var actual;
     var corrected;
     var expected;
@@ -174,7 +174,7 @@ describe('gulp-checktextdomain', function() {
 
     gulp.src(fixtures('correct-domain.php'))
       .pipe(checktextdomain(options))
-      .pipe(sassert.first(function(d) {
+      .pipe(sassert.first(function (d) {
         //File is correct, should report no errors
         actual = JSON.parse(fs.readFileSync('.correct-domain.json'));
         actual[path.join(__dirname, 'temp/correct-domain.php')].length.should.equal(0);
@@ -186,7 +186,7 @@ describe('gulp-checktextdomain', function() {
 
   });
 
-  it('6) Should review plurals', function(done) {
+  it('6) Should review plurals', function (done) {
     var actual;
     var corrected;
     var expected;
@@ -199,7 +199,7 @@ describe('gulp-checktextdomain', function() {
 
     gulp.src(fixtures('plurals.php'))
       .pipe(checktextdomain(options))
-      .pipe(sassert.first(function(d) {
+      .pipe(sassert.first(function (d) {
         //File is correct, should report no errors
         actual = JSON.parse(fs.readFileSync('.plurals.json'));
         actual[path.join(__dirname, 'temp/plurals.php')].length.should.equal(0);
@@ -212,49 +212,49 @@ describe('gulp-checktextdomain', function() {
   });
 
   it('7) Should emit error on streamed file', function (done) {
-      var options = {
-        force: true,
-        text_domain: 'my-domain',
-        create_report_file: true,
-        keywords: keywords
-      };
-      gulp.src(fixtures('plurals.php'), { buffer: false })
-        .pipe(checktextdomain(options))
-        .on('error', function (err) {
-          err.message.should.eql('Streaming not supported');
-          done();
-       });
-   });
+    var options = {
+      force: true,
+      text_domain: 'my-domain',
+      create_report_file: true,
+      keywords: keywords
+    };
+    gulp.src(fixtures('plurals.php'), { buffer: false })
+      .pipe(checktextdomain(options))
+      .on('error', function (err) {
+        err.message.should.eql('Streaming not supported');
+        done();
+      });
+  });
 
-   it('8) Should emit error when there is no text_domain', function (done) {
-      var options = {
-        force: true,
-        create_report_file: true,
-        keywords: keywords
-      };
-      gulp.src(fixtures('plurals.php'))
-        .pipe(checktextdomain(options))
-        .on('error', function (err) {
-          err.message.should.eql('Text domain not provided.');
-          done();
-       });
-   });
+  it('8) Should emit error when there is no text_domain', function (done) {
+    var options = {
+      force: true,
+      create_report_file: true,
+      keywords: keywords
+    };
+    gulp.src(fixtures('plurals.php'))
+      .pipe(checktextdomain(options))
+      .on('error', function (err) {
+        err.message.should.eql('Text domain not provided.');
+        done();
+      });
+  });
 
-   it('9) Should emit error when there is no keywords', function (done) {
-      var options = {
-        force: true,
-        create_report_file: true,
-        text_domain: 'my-domain'
-      };
-      gulp.src(fixtures('plurals.php'))
-        .pipe(checktextdomain(options))
-        .on('error', function (err) {
-          err.message.should.eql('No keywords specified.');
-          done();
-       });
-   });
+  it('9) Should emit error when there is no keywords', function (done) {
+    var options = {
+      force: true,
+      create_report_file: true,
+      text_domain: 'my-domain'
+    };
+    gulp.src(fixtures('plurals.php'))
+      .pipe(checktextdomain(options))
+      .on('error', function (err) {
+        err.message.should.eql('No keywords specified.');
+        done();
+      });
+  });
 
-  it('10) Should review that the domain (set as a constant) is ok', function(done) {
+  it('10) Should review that the domain (set as a constant) is ok', function (done) {
     var actual;
     var corrected;
     var expected;
@@ -267,7 +267,7 @@ describe('gulp-checktextdomain', function() {
 
     gulp.src(fixtures('correct-domain-constant.php'))
       .pipe(checktextdomain(options))
-      .pipe(sassert.first(function(d) {
+      .pipe(sassert.first(function (d) {
         //File is correct, should report no errors
         actual = JSON.parse(fs.readFileSync('.correct-domain-constant.json'));
         actual[path.join(__dirname, 'temp/correct-domain-constant.php')].length.should.equal(0);
